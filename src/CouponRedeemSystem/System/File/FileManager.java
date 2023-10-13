09a316420dbbc0427b916935004a3ea7bfab08c3
T04Yeung <tonyyeung595@gmail.com>
2023-10-13 14:56:10 +0800

    File Manager Base

--- src/CouponRedeemSystem/System/File/FileManager.java
+++ src/CouponRedeemSystem/System/File/FileManager.java
@@ -1,5 +1,43 @@
 package CouponRedeemSystem.System.File;
 
-public class FileManager {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import CouponRedeemSystem.System.File.model.TextFile;
+
+public class FileManager {
+	private static FileManager instance;
+	
+	private FileManager() {}
+	
+	public static FileManager getInstance() {
+		instance = instance == null ? new FileManager() : instance;
+		return instance;
+	}
+	
+	private File getTextFile(String dirName, TextFile file) throws IOException {
+		String dirPathString = "../../../../Data/" + dirName;
+		File folder = new File(dirPathString);
+		if(!folder.exists()) {
+			folder.mkdir();
+		}
+		File realFile = new File(folder.getPath() + file.getFileName());
+		if (!realFile.exists()) {
+			realFile.createNewFile();
+		}
+		return realFile; 
+	}
+	
+	public void modifyFile(String dirName, TextFile file) throws IOException {
+		File textFile = getTextFile(dirName, file);
+		FileWriter fileWriter = new FileWriter(textFile);
+		fileWriter.write(file.getFileContent());
+		fileWriter.close();
+	}
+	
+	public void deleteFile(String dirName, TextFile file) throws IOException {
+		File textFile = getTextFile(dirName, file);
+		textFile.delete();
+	}
 }
--- src/CouponRedeemSystem/System/File/model/TextFile.java
+++ src/CouponRedeemSystem/System/File/model/TextFile.java
@@ -1,5 +1,11 @@
 package CouponRedeemSystem.System.File.model;
 
 public abstract class TextFile {
-
+	String fileName;
+	String fileContent;
+	
+	public String getFileName() {return fileName;}
+	public void setFiName(String fileName) {this.fileName = fileName;}
+	public String getFileContent() {return fileContent;}
+	public void setFileContent(String fileContent) {this.fileContent = fileContent;}
 }
