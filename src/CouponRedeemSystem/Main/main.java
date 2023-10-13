45f4f63c2e6e3c20e28b264deabfa0132aac09ec
T04Yeung <tonyyeung595@gmail.com>
2023-10-13 16:11:20 +0800

    ...

--- src/CouponRedeemSystem/Main/main.java
+++ src/CouponRedeemSystem/Main/main.java
@@ -8,10 +8,7 @@ import CouponRedeemSystem.System.File.model.TextFile;
 public class main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		FileManager mgr = FileManager.getInstance();
-		TextFile textFile = new TextFile("0001", "Tony:1111");
-		mgr.modifyFile("Password", textFile);
+
 	}
 
 }
