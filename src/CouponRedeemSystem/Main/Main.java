3946a11a75f0aedd267cc4ba7479eb6e9aa2f60a
T04Yeung <tonyyeung595@gmail.com>
2023-11-10 12:46:25 +0800

    JSON

--- src/CouponRedeemSystem/Main/Main.java
+++ src/CouponRedeemSystem/Main/Main.java
@@ -1,5 +1,6 @@
 package CouponRedeemSystem.Main;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.beanutils.LazyDynaBean;
@@ -10,12 +11,6 @@ import net.sf.json.JSONObject;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		CRSJsonFileManager mgr = CRSJsonFileManager.getInstance();
-		LazyDynaBean bean = new LazyDynaBean();
-		bean.set("Tony", "Good");
-		bean.set("Number", 1);
-		bean.set("3343", false);
-		mgr.modifyJSON("Test", "Bean Test", bean);
-		System.out.print(bean.toString());
+		
 	}
 }
