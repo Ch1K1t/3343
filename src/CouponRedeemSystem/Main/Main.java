0409fd2ec0b37efef6be0b8668c563625739d424
T04Yeung <tonyyeung595@gmail.com>
2023-11-10 12:26:50 +0800

    bug fix

--- src/CouponRedeemSystem/Main/Main.java
+++ src/CouponRedeemSystem/Main/Main.java
@@ -2,14 +2,20 @@ package CouponRedeemSystem.Main;
 
 import java.io.IOException;
 
+import org.apache.commons.beanutils.LazyDynaBean;
+
 import CouponRedeemSystem.System.File.CRSJsonFileManager;
 import net.sf.json.JSONObject;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
+		CRSJsonFileManager mgr = CRSJsonFileManager.getInstance();
+		LazyDynaBean bean = new LazyDynaBean();
+		bean.set("Tony", "Good");
+		bean.set("Number", 1);
+		bean.set("3343", false);
+		mgr.modifyJSON("Test", "Bean Test", bean);
+		System.out.print(bean.toString());
 	}
-
 }
