5e41853b606c8bffbd1a26792b49f5b9180d65a6
HA1188 <ansonh118@gmail.com>
2023-11-17 15:27:59 +0800

    modified:   .classpath 	modified:   bin/CouponRedeemSystem/Account/model/Account.class 	modified:   bin/CouponRedeemSystem/Coupon/model/Coupon.class 	modified:   bin/CouponRedeemSystem/Main/Main.class 	modified:   bin/CouponRedeemSystem/Page/HomePage.class 	modified:   bin/CouponRedeemSystem/Page/SigninPage.class 	modified:   bin/CouponRedeemSystem/Page/SignupPage.class 	modified:   bin/CouponRedeemSystem/Page/model/Page.class 	modified:   bin/CouponRedeemSystem/System/File/CRSJsonFileManager.class 	modified:   src/CouponRedeemSystem/Account/AccountManager.java 	modified:   src/CouponRedeemSystem/Account/model/Account.java 	modified:   src/CouponRedeemSystem/Coupon/model/Coupon.java 	modified:   src/CouponRedeemSystem/Coupon/model/NormalCoupon.java 	modified:   src/CouponRedeemSystem/Main/Main.java 	modified:   src/CouponRedeemSystem/Page/HomePage.java 	new file:   src/CouponRedeemSystem/Page/ProfilePage.java 	new file:   src/CouponRedeemSystem/Page/SearchPage.java 	new file:   src/CouponRedeemSystem/Page/ShopListPage.java 	modified:   src/CouponRedeemSystem/Page/SigninPage.java 	modified:   src/CouponRedeemSystem/Page/SignupPage.java 	modified:   src/CouponRedeemSystem/Page/model/Page.java

--- src/CouponRedeemSystem/Main/Main.java
+++ src/CouponRedeemSystem/Main/Main.java
@@ -1,27 +1,11 @@
 package CouponRedeemSystem.Main;
+
 import CouponRedeemSystem.Page.HomePage;
-import CouponRedeemSystem.System.File.CRSJsonFileManager;
-import net.sf.json.JSONObject;
-
 import java.io.IOException;
-import java.util.HashMap;
-
-import org.apache.commons.beanutils.LazyDynaBean;
 
 public class Main {
+
   public static void main(String[] args) throws IOException {
-	CRSJsonFileManager mgr = CRSJsonFileManager.getInstance();
-	LazyDynaBean bean = new LazyDynaBean();
-	bean.set("Tony", "Good");
-	bean.set("Number", 1);
-	bean.set("3343", false);
-	mgr.modifyJSON("Test01", "Bean Test", bean);
-	System.out.print(bean.toString());
-	
-	JSONObject jsonObject = new JSONObject();
-	jsonObject.put("Apple user", "Hendry");
-	JSONObject feature = new JSONObject();
-	feature.putAll(new HashMap<>());
     new HomePage().execute();
   }
 }
--- src/CouponRedeemSystem/Page/SigninPage.java
+++ src/CouponRedeemSystem/Page/SigninPage.java
@@ -1,11 +1,56 @@
 package CouponRedeemSystem.Page;
 
 import CouponRedeemSystem.Page.model.Page;
+import java.util.Scanner;
 
-public class SigninPage implements Page {
+public class SigninPage extends Page {
 
-  @Override
+  public void getInstructions() {
+    System.out.println();
+    System.out.println("Please input the command e.g. !CreateCoupon:");
+    System.out.println("1. CreateCoupon");
+    System.out.println("2. RedeemCoupon");
+    System.out.println("3. Signout");
+    System.out.println("4. Exit");
+    System.out.println();
+  }
+
   public void execute() {
-    System.out.println("Signin");
+    System.out.println();
+    System.out.println("Please input your username:");
+    String username = s.nextLine();
+    System.out.println("Please input your password:");
+    String password = s.nextLine();
+    System.out.println();
+
+    if (username.equals("admin") && password.equals("admin")) {
+      System.out.println("Welcome " + username + "!");
+
+      String cmd;
+      do {
+        getInstructions();
+        cmd = s.nextLine().toLowerCase();
+
+        switch (cmd) {
+          case "!createcoupon":
+            System.out.println("Coupon created");
+            break;
+          case "!redeemcoupon":
+            System.out.println("Coupon redeemed");
+            break;
+          case "!signout":
+            System.out.println("Signout");
+            break;
+          case "!exit":
+            exit();
+            break;
+          default:
+            System.out.println("Unknown command");
+            break;
+        }
+      } while (!cmd.equals("!signout"));
+    } else {
+      System.out.println("Invalid username or password");
+    }
   }
 }
