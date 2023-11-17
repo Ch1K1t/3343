b0f383893133715bed7b49e46f67533cbe8b0f06
Hendry Li <119316431+LiHendryWijaya@users.noreply.github.com>
2023-11-17 23:33:02 +0800

    modified:   Data/Coupon/17897.json 	modified:   bin/CouponRedeemSystem/Account/AccountManager.class 	modified:   bin/CouponRedeemSystem/Account/model/Account.class 	modified:   bin/CouponRedeemSystem/Coupon/CouponManager.class 	modified:   bin/CouponRedeemSystem/Coupon/model/Coupon.class 	modified:   bin/CouponRedeemSystem/Main/Main.class 	modified:   src/CouponRedeemSystem/Account/AccountManager.java 	modified:   src/CouponRedeemSystem/Account/model/Account.java

--- src/CouponRedeemSystem/Account/AccountManager.java
+++ src/CouponRedeemSystem/Account/AccountManager.java
@@ -1,12 +1,22 @@
 package CouponRedeemSystem.Account;
 
 import CouponRedeemSystem.Account.model.Account;
+import CouponRedeemSystem.Coupon.model.Coupon;
+import CouponRedeemSystem.System.File.CRSJsonFileManager;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AccountManager {
 
   private AccountManager instance;
+	CRSJsonFileManager JsonFileManager = CRSJsonFileManager.getInstance();
 
   private AccountManager() {}
 
@@ -17,20 +27,74 @@ public class AccountManager {
     return instance;
   }
 
-  public List<Account> list() {
-    //TODO
-    return new ArrayList<Account>();
-  }
+	// Save account details to account folder
+	public void saveAccountDetailsToJsonFolder(Account account) {
+		JSONObject accountJson = convertAccountToJson(account);
 
-  public void create() {
-    //TODO
-  }
+		// Combine the directory path and file name to create the file path
+		String userNameString = account.getUserName();
+        String filePath =  "Data/Account/" + userNameString;
 
-  public void modify() {
-    //TODO
-  }
+        // Write the JSONObject to the file
+        try {
+            JsonFileManager.modifyJSON(userNameString, filePath, accountJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
 
-  public void delete() {
-    //TODO
-  }
+	// Convert account to JSONObject
+    private JSONObject convertAccountToJson(Account account) {
+
+        JSONObject accountJson = new JSONObject();
+		accountJson.put("userName", account.getUserName());
+        accountJson.put("age", account.getAge());
+        accountJson.put("telNo", account.getTelNo());
+        accountJson.put("points", account.getPoints());
+        accountJson.put("dateOfBirth", account.getDateOfBirth());
+
+        // Convert list of coupons to JSONArray
+        JSONArray couponsIDArray = new JSONArray();
+        for (String couponId : account.getCouponIDs()) {
+            // Add coupon details to couponJson
+            couponsIDArray.add(couponId);
+        }
+        accountJson.put("coupons", couponsIDArray);
+
+        return accountJson;
+    }
+
+	// Load account details from JSON file
+	private Account loadAccountDetailsFromFolder(String UserName) {
+		try {
+			JSONObject accountJson = JsonFileManager.searchJSON("Data/Account/"+UserName, null);
+			if (!accountJson.isEmpty()) {
+				return extractAccountFromJson(accountJson);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Extract the JSON to an Account object
+    private Account extractAccountFromJson(JSONObject accountJson) {
+		String userName = accountJson.getString("userName");
+        int age = accountJson.getInt("age");
+        int telNo = accountJson.getInt("telNo");
+        double points = accountJson.getDouble("points");
+        String dateOfBirth = accountJson.getString("dateOfBirth"); 
+		JSONArray couponIDs = accountJson.getJSONArray("coupons");
+		List<String> couponIDList = new ArrayList<>();
+        for (int i = 0; i < couponIDs.size(); i++) {
+            couponIDList.add(couponIDs.getString(i));
+        }
+        // You may need to handle parsing exceptions based on your specific needs
+        try {
+            return new Account(userName, age, telNo, points, dateOfBirth,couponIDList);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null; // Handle the exception according to your needs
+        }
+    }
+
 }
