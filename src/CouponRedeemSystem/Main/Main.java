6b4133e61f5ec185baaadf49cb4bd39598156cf8
HA1188 <ansonh118@gmail.com>
2023-11-18 23:00:20 +0800

    modified:   .vscode/settings.json 	modified:   Data/Account/hendry.json 	modified:   Data/Account/tony.json 	modified:   bin/CouponRedeemSystem/Main/Main.class 	modified:   bin/CouponRedeemSystem/System/File/CRSJsonFileManager.class 	modified:   src/CouponRedeemSystem/Main/Main.java

--- src/CouponRedeemSystem/Main/Main.java
+++ src/CouponRedeemSystem/Main/Main.java
@@ -2,10 +2,6 @@ package CouponRedeemSystem.Main;
 
 import CouponRedeemSystem.Account.AccountManager;
 import CouponRedeemSystem.Account.model.Account;
-import CouponRedeemSystem.Page.HomePage;
-import CouponRedeemSystem.System.File.CRSJsonFileManager;
-import net.sf.json.JSONObject;
-
 import java.io.IOException;
 import java.text.ParseException;
 
@@ -16,17 +12,17 @@ public class Main {
     // JSONObject json = jsonFileManager.searchJSON("09823403.json", null);
     // System.out.println(json);
     //new HomePage().execute();
-    /*
-    Account testUser = new Account("hendry", 20, 213,"2003-01-01");
-    AccountManager.getInstance().create(testUser);
-    testUser.addPoints(1000);
-    testUser.deductPoints(50);
-    testUser.addCouponID("testCoupon");
-    testUser.addCouponID("testCoupon2");
-    testUser.deleteCouponID("testCoupon");
-    AccountManager.getInstance().update(testUser);
-    */
+
+    // Account testUser = new Account("hendry", 20, 213,"2003-01-01");
+    // AccountManager.getInstance().create(testUser);
+    // testUser.addPoints(1000);
+    // testUser.deductPoints(50);
+    // testUser.addCouponID("testCoupon");
+    // testUser.addCouponID("testCoupon2");
+    // testUser.deleteCouponID("testCoupon");
+    // AccountManager.getInstance().update(testUser);
+
     Account testUser2 = AccountManager.getInstance().getAccount("tony");
-    System.out.println(testUser2.getUserName() + testUser2.getTelNo()); 
+    System.out.println(testUser2.getUserName() + testUser2.getTelNo());
   }
 }
