624314eccc846c86aad4ce73316348ebd21004c5
kelvinlam0621 <kitleunglam@gmail.com>
2023-11-18 23:33:37 +0800

    coupon type update; implemented json to coupon type

--- src/CouponRedeemSystem/Main/Main.java
+++ src/CouponRedeemSystem/Main/Main.java
@@ -2,20 +2,26 @@ package CouponRedeemSystem.Main;
 
 import CouponRedeemSystem.Account.AccountManager;
 import CouponRedeemSystem.Account.model.Account;
+import CouponRedeemSystem.Coupon.CouponManager;
+import CouponRedeemSystem.Coupon.model.Coupon;
+import CouponRedeemSystem.Coupon.model.RedeemableCoupon;
 import CouponRedeemSystem.Page.HomePage;
 import CouponRedeemSystem.System.File.CRSJsonFileManager;
 import net.sf.json.JSONObject;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Date;
 
 public class Main {
 
   public static void main(String[] args) throws IOException, ParseException {
-    // CRSJsonFileManager jsonFileManager = CRSJsonFileManager.getInstance();
-    // JSONObject json = jsonFileManager.searchJSON("09823403.json", null);
-    // System.out.println(json);
-    //new HomePage().execute();
+    CRSJsonFileManager jsonFileManager = CRSJsonFileManager.getInstance();
+    CouponManager manager = CouponManager.getInstance();
+    manager.create(new RedeemableCoupon(0, null, new Date(), "798", false), "Redeemable");
+    JSONObject json = jsonFileManager.searchJSON("09823403.json", null);
+    System.out.println(json);
+    // new HomePage().execute();
     /*
     Account testUser = new Account("hendry", 20, 213,"2003-01-01");
     AccountManager.getInstance().create(testUser);
@@ -26,7 +32,7 @@ public class Main {
     testUser.deleteCouponID("testCoupon");
     AccountManager.getInstance().update(testUser);
     */
-    Account testUser2 = AccountManager.getInstance().getAccount("tony");
-    System.out.println(testUser2.getUserName() + testUser2.getTelNo()); 
+    // Account testUser2 = AccountManager.getInstance().getAccount("tony");
+    // System.out.println(testUser2.getUserName() + testUser2.getTelNo()); 
   }
 }
