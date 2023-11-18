46a054139ea8a39f47c3d7d8218b064e4fc129d3
kelvinlam0621 <kitleunglam@gmail.com>
2023-11-18 23:35:40 +0800

    fix import

--- src/CouponRedeemSystem/Main/Main.java
+++ src/CouponRedeemSystem/Main/Main.java
@@ -1,11 +1,10 @@
 package CouponRedeemSystem.Main;
 
-import CouponRedeemSystem.Account.AccountManager;
-import CouponRedeemSystem.Account.model.Account;
 import CouponRedeemSystem.Coupon.CouponManager;
-import CouponRedeemSystem.Coupon.model.Coupon;
 import CouponRedeemSystem.Coupon.model.RedeemableCoupon;
-import CouponRedeemSystem.Page.HomePage;
+import CouponRedeemSystem.System.File.CRSJsonFileManager;
+import net.sf.json.JSONObject;
+
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
