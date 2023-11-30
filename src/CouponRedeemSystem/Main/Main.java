f715112b99ad1b079d6120bfebdeb7189669ab99
HA1188 <ansonh118@gmail.com>
2023-12-01 00:02:08 +0800

    deleted:    Data/Account/admin.json 	deleted:    Data/Account/shop.json 	deleted:    Data/Account/staff1.json 	deleted:    Data/Account/staff2.json 	deleted:    Data/Account/user.json 	deleted:    Data/Coupon/Purchasable/P1.json 	deleted:    Data/Coupon/Purchasable/P2.json 	deleted:    Data/Coupon/Redeemable/R1.json 	deleted:    Data/Coupon/Redeemable/R2.json 	deleted:    Data/Discount/discount1.json 	deleted:    Data/Discount/discount2.json 	deleted:    Data/Password/ReferenceTable.json 	deleted:    Data/Shop/shop1.json 	deleted:    Data/Shop/shop2.json 	modified:   bin/CouponRedeemSystem/Main/Main.class 	modified:   src/CouponRedeemSystem/Main/Main.java

--- src/CouponRedeemSystem/Main/Main.java
+++ src/CouponRedeemSystem/Main/Main.java
@@ -6,7 +6,7 @@ import CouponRedeemSystem.System.Util.Util;
 public class Main {
 
   public static void main(String[] args) {
-    new HomePage().execute();
+    // new HomePage().execute();
     // Util.clearSystem();
     // Util.initializeSystem();
   }
